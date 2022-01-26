586c0cb6f0400ecc2c29dc5bd214205bd17b9fb0
MD: Suvo <santo35702@gmail.com>
2022-01-26 16:04:23 +0600

    Shop: Cart, Checkout, AboutUs, Wishlist, Compare, ContactUs, FAQ & NotFound Page default created

---
+++ app/Http/Livewire/Frontend/Cart/IndexPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Frontend\Cart;
+
+use Livewire\Component;
+
+class IndexPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.cart.index-page');
+    }
+}
---
+++ app/Http/Livewire/Frontend/Wishlist/IndexPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Frontend\Wishlist;
+
+use Livewire\Component;
+
+class IndexPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.wishlist.index-page');
+    }
+}
