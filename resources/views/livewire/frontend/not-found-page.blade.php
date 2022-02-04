f151c03660afaa6d6e452107f094ed44fd0cc737
MD: Suvo <santo35702@gmail.com>
2022-02-04 15:00:46 +0600

    sample content set done

--- resources/views/livewire/frontend/not-found-page.blade.php
+++ resources/views/livewire/frontend/not-found-page.blade.php
@@ -1,3 +1,15 @@
 <div id="page-content">
-    <h1>404...!! Page Not Found content.</h1>
+    <!-- Lookbook Start -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="empty-page-content text-center">
+                    <h1>404 Page Not Found</h1>
+                    <p>The page you requested does not exist.</p>
+                    <p><a href="http://laravel.net/products" class="btn btn--has-icon-after">Continue shopping <i class="fa fa-caret-right" aria-hidden="true"></i></a></p>
+                  </div>
+            </div>
+        </div>
+    </div>
+    <!-- Lookbook Start -->
 </div>
